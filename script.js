9f8a2268edeff55a1c5e67198e6a45cbbe855fbe
JBTechDev <jbtechdev@gmail.com>
2019-10-04 18:45:30 -0700

    added js

--- script.js
+++ script.js
@@ -5,19 +5,37 @@ var securePassword = document.getElementById("securePassword");
 newPasswordBtn.addEventListener("click", function(e) {
     e.preventDefault();
     
+    // Get password length
     var passwordLength = document.getElementById("password-length").value;
-
+    // Check if user selected at least one option
+    if (passwordLength < 8 || passwordLength > 128) {
+        alert("Value must be between 8 - 128")
+        return;
+    }
+    // Characters to be used in the password
     var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+={}|[]\;':.,/?><`~";
+    // Set password variable to empty
     var password = "";
-
-    // Loop through the values variable
+    // Loop through the values variable and set random characters for the length of the password
     for (i = 0; i <= passwordLength; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
     }
-
-    securePassword.textContent = "Your secure password is " + password;
+    // Display new randomly generated password 
+    securePassword.textContent = "Your secure password is: " + password;
 
 })
+
+
+
+copyPasswordBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.execCommand("copy", false,
+    document.getElementById("securePassword").parseFromString.select());
+    debugger;
+});
+
+
 
 
 // validate user input & ensure at least one character type is selected
